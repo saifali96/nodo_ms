366cea240a99a3d7522205e336649544609a4f32
Saif Ali <Saifincreation@gmail.com>
2022-05-16 00:08:36 +0200

    refactor: Customer api

--- customer/src/api/customer.js
+++ customer/src/api/customer.js
@@ -44,7 +44,7 @@ module.exports = (app) => {
 	
 			const { data } = await service.AddNewAddress( _id ,{ street, postalCode, city,country});
 	
-			return res.status(200).json(data);
+			return res.status(200).json({ success: true, message: data });
 
 		} catch (err) {
 			next(err)
@@ -59,10 +59,10 @@ module.exports = (app) => {
 		try {
 			const { _id } = req.user;
 			const { data } = await service.GetProfile({ _id });
-			return res.status(200).json(data);
+			return res.status(200).json({ success: true, message: data });
 			
 		} catch (err) {
-			next(err)
+			next(err);
 		}
 	});
 	 
@@ -73,7 +73,7 @@ module.exports = (app) => {
 			const { _id } = req.user;
 		   const { data } = await service.GetShoppingDetails(_id);
 	
-		   return res.status(200).json(data);
+		   return res.status(200).json({ success: true, message: data });
 			
 		} catch (err) {
 			next(err)
@@ -84,7 +84,7 @@ module.exports = (app) => {
 		try {
 			const { _id } = req.user;
 			const { data } = await service.GetWishList( _id);
-			return res.status(200).json(data);
+			return res.status(200).json({ success: true, message: data });
 			
 		} catch (err) {
 			next(err)
